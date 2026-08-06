49441313ee525d0e9b68862aabbd29b80a4d9f56
pathakjiravi23-wq <pathakjiravi23@gmail.com>
2026-08-06 23:38:38 +0530

    personal details rolled back

--- database.py
+++ database.py
@@ -6,10 +6,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # 1. DATABASE CONNECTION SETUP
 # ==========================================
 # Format: postgresql+psycopg2://<username>:<password>@<host>:<port>/<database_name>
-DATABASE_URL = "postgresql+psycopg2://postgres:Ravi123@localhost:5432/practice_db"
+DataBase = os.environ["DATABASE_URL"]
 # create_engine manages the live connection pool to PostgreSQL
 # echo=True prints the generated SQL queries to your terminal (great for learning!)
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(DataBase, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
